f5988eebcee0208780e3ed8d23e683bf1daa4d4d
Henry Thuler <henrythuler2003@outlook.com>
2024-05-20 14:22:26 -0300

    feat(user): Create user implemented

--- src/main/java/com/thuler/socialmedia/controller/UserController.java
+++ src/main/java/com/thuler/socialmedia/controller/UserController.java
@@ -1,14 +1,14 @@
 package com.thuler.socialmedia.controller;
 
 import com.thuler.socialmedia.dto.UserDTO;
+import com.thuler.socialmedia.model.User;
 import com.thuler.socialmedia.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -17,6 +17,13 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+
+    @PostMapping
+    public ResponseEntity<UserDTO> create(@RequestBody UserDTO user){
+        UserDTO newUser = userService.create(user);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newUser.id()).toUri();
+        return ResponseEntity.created(location).body(newUser);
+    }
 
     @GetMapping
     public ResponseEntity<List<UserDTO>> findAll(){
--- src/main/java/com/thuler/socialmedia/service/UserService.java
+++ src/main/java/com/thuler/socialmedia/service/UserService.java
@@ -2,7 +2,9 @@ package com.thuler.socialmedia.service;
 
 import com.thuler.socialmedia.dto.UserDTO;
 import com.thuler.socialmedia.exception.NotFoundException;
+import com.thuler.socialmedia.model.User;
 import com.thuler.socialmedia.repository.UserRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +15,12 @@ public class UserService {
 
     @Autowired
     private UserRepository userRepository;
+
+    public UserDTO create(UserDTO user){
+        User newUser = new User();
+        BeanUtils.copyProperties(user, newUser);
+        return new UserDTO(userRepository.save(newUser));
+    }
 
     public UserDTO findById(String id){
         return new UserDTO(userRepository.findById(id).orElseThrow(() -> new NotFoundException("User")));
